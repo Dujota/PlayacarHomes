bae01cdd6d74cb9c91eb5c3d3d779cda97d5bc72
Denis S Dujota <denisdujota@gmail.com>
2023-08-31 22:33:43 -0500

    update the form to submit

--- components/common/forms/ContactForm.tsx
+++ components/common/forms/ContactForm.tsx
@@ -1,46 +1,108 @@
+import { useState } from 'react';
+
 type ContactFormType = {
   fullNameInput?: string;
   buttonText?: string;
 };
 
+// const bg =
+//   'box-border h-[571px] w-[403px] rounded-2xl border-[1px] border-solid border-gray shadow-[0px_40px_64px_-32px_rgba(15,_15,_15,_0.1)] [backdrop-filter:blur(32px)] [background:linear-gradient(83.59deg,_#fcfcfd,_rgba(252,_252,_253,_0.83))]';
 const bg =
-  'box-border h-[571px] w-[403px] rounded-2xl border-[1px] border-solid border-gray shadow-[0px_40px_64px_-32px_rgba(15,_15,_15,_0.1)] [backdrop-filter:blur(32px)] [background:linear-gradient(83.59deg,_#fcfcfd,_rgba(252,_252,_253,_0.83))]';
+  'box-border w-[403px] rounded-2xl border-[1px] border-solid border-gray shadow-[0px_40px_64px_-32px_rgba(15,_15,_15,_0.1)] [backdrop-filter:blur(32px)] [background:linear-gradient(83.59deg,_#fcfcfd,_rgba(252,_252,_253,_0.83))]';
+
+const inputStyles = 'p-2 border-1 border-solid bg-[rgba(119,126,144,0.05)] border-[rgba(114,151,175,0.25)] rounded';
 
 const ContactForm = ({ fullNameInput, buttonText }: ContactFormType) => {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [message, setMessage] = useState('');
+  const [email, setEmail] = useState('');
+  const [errors, setErrors] = useState({ email: '', phoneNumber: '', message: '' });
+
+  const validateEmail = (inputEmail) => {
+    // Regular expression to check common email format
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return regex.test(inputEmail);
+  };
+
+  const validate = () => {
+    let hasError = false;
+    let newErrors = { email: '', phoneNumber: '', message: '' };
+
+    if (!phoneNumber.trim() || !/^\+?[\d\s-]{10,15}$/.test(phoneNumber.replace(/[-\s]+/g, '').trim())) {
+      newErrors.phoneNumber = 'Valid phone number is required.';
+      hasError = true;
+    }
+
+    if (!message.trim()) {
+      newErrors.message = 'Message is required.';
+      hasError = true;
+    }
+    if (!email.trim() || !validateEmail(email)) {
+      newErrors.email = 'Valid email is required.';
+      hasError = true;
+    }
+
+    setErrors(newErrors);
+    return !hasError;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (validate()) {
+      alert(`Email: ${email}\nPhone Number: ${phoneNumber}\nMessage: ${message}`);
+      setPhoneNumber('');
+      setMessage('');
+      setEmail('');
+    }
+  };
+
   return (
-    <div className={'font-poppins relative h-[571px] w-[403px] text-left text-5xl text-black' + ' ' + bg}>
-      <div className='absolute left-[27px] top-[32px] flex flex-col items-center justify-start gap-[32px]'>
-        <div className='flex flex-col items-start justify-start gap-[16px]'>
-          <h2 className='font-inherit relative m-0 inline-block w-[350px] font-medium text-inherit'>Ask More Details</h2>
-          <div className='relative inline-block w-[350px] text-sm leading-[170%] text-grey'>
-            Interested in this property? Get in touch with the owner directly using the form below. Submit your inquiry, and the owner will contact you.
+    <div className={'font-poppins relative w-[403px] rounded-2xl p-[32px] text-left text-5xl text-black ' + bg}>
+      <form onSubmit={handleSubmit}>
+        <div className='flex flex-col items-center justify-start gap-[32px]'>
+          <div className='flex flex-col items-start justify-start gap-[16px]'>
+            <h2 className='font-inherit relative m-0 inline-block w-[350px] font-medium text-inherit'>Ask More Details</h2>
+            <div className='relative inline-block w-[350px] text-sm leading-[170%] text-grey'>
+              Interested in this property? Get in touch with the owner directly using the form below. Submit your inquiry, and the owner will contact you.
+            </div>
+          </div>
+          <div className='flex flex-col items-start justify-start gap-[32px] text-sm text-grey'>
+            <div className='flex flex-col items-start justify-start gap-[12px]'>
+              <input
+                className={`font-poppins relative h-[60px] w-[350px] bg-[transparent] text-sm font-light [border:none] ${inputStyles}`}
+                type='email'
+                placeholder='Email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              {errors.email && <div className='text-red-500'>{errors.email}</div>}
+
+              <input
+                className={`font-poppins relative h-[60px] w-[350px] bg-[transparent] text-sm font-light [border:none] ${inputStyles}`}
+                type='tel'
+                placeholder='Phone Number'
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
+              {errors.phoneNumber && <div className='text-red-500'>{errors.phoneNumber}</div>}
+
+              <textarea
+                className={`font-poppins relative h-40 w-[350px] bg-[transparent] text-sm font-light [border:none] ${inputStyles}`}
+                placeholder='Your Message'
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              {errors.message && <div className='text-red-500'>{errors.message}</div>}
+            </div>
+            <button className='flex cursor-pointer flex-row items-center justify-center bg-blue pb-[11px] pl-[124px] pr-[123px] pt-3 [border:none]' type='submit'>
+              <div className='font-poppins relative text-left text-sm font-medium text-white'>Send Message</div>
+            </button>
           </div>
         </div>
-        <div className='flex flex-col items-start justify-start gap-[32px] text-sm text-grey'>
-          <div className='flex flex-col items-start justify-start gap-[12px]'>
-            <input className='font-poppins relative h-[60px] w-[350px] bg-[transparent] text-sm font-light [border:none]' type='text' placeholder='Phone Number' />
-            <textarea className='font-poppins relative h-40 w-[350px] bg-[transparent] text-sm font-light [border:none]' placeholder='Your Message' />
-          </div>
-          <button className='flex cursor-pointer flex-row items-center justify-center bg-blue pb-[11px] pl-[124px] pr-[123px] pt-3 [border:none]'>
-            <div className='font-poppins relative text-left text-sm font-medium text-white'>Send Message</div>
-          </button>
-        </div>
-      </div>
+      </form>
     </div>
   );
 };
 
 export default ContactForm;
-
-// {/* <div className='absolute left-[calc(50%_-_201.5px)] top-[0px] ' /> */}
-
-//{
-/* <div className='relative h-[60px] w-[350px]'>
-              <div className='absolute left-[0px] top-[0px] box-border h-[60px] w-[350px] border-[1px] border-solid border-lightslategray bg-slategray-200' />
-              <div className='absolute left-[12px] top-[20px] font-light'>{fullNameInput}</div>
-            </div>
-            <div className='relative h-[60px] w-[350px]'>
-              <div className='absolute left-[0px] top-[0px] box-border h-[60px] w-[350px] border-[1px] border-solid border-lightslategray bg-slategray-200' />
-              <div className='absolute left-[12px] top-[20px] font-light'>{buttonText}</div>
-            </div> */
-//}
