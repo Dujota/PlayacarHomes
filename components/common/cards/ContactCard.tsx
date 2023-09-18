f57ad2fa1d443acd38a115709312f24a6f06d74e
Denis S Dujota <denisdujota@gmail.com>
2023-09-17 20:46:30 -0500

    remove dead code

--- components/common/cards/ContactCard.tsx
+++ components/common/cards/ContactCard.tsx
@@ -6,21 +6,6 @@ export type ContactCardProps = {
   description: string;
 };
 
-// const ContactCard: React.FC<ContactCardProps> = ({ icon, title, description }) => {
-//   return (
-//     <article className='m-6 flex w-1/3 flex-col items-center justify-start gap-4 p-2 md:w-full'>
-//       <figure className='flex-shrink-0'>
-//         <Image width={200} height={200} className='relative w-16 md:w-20' alt={title} src={icon} />
-//         <figcaption className='sr-only'>{title}</figcaption>
-//       </figure>
-//       <div className='flex w-full flex-col items-center justify-start gap-2'>
-//         <p className='m-0 w-full text-center font-medium md:w-2/3 lg:w-2/3'>{title}</p>
-//         <p className='m-0 w-full text-center text-grey md:w-2/3 lg:w-2/3'>{description}</p>
-//       </div>
-//     </article>
-//   );
-// };
-
 const ContactCard: React.FC<ContactCardProps> = ({ icon, title, description }) => {
   return (
     <article className='mx-8 flex w-1/4 flex-col items-center justify-start gap-4 p-2 md:w-2/3'>
--- components/common/footer/ContactDetails.tsx
+++ components/common/footer/ContactDetails.tsx
@@ -4,18 +4,6 @@ interface ContactDetailsProps {
   contactInfo: ContactCardProps[];
 }
 
-// export default function ContactDetails({ contactInfo }: ContactDetailsProps) {
-//   return (
-//     <div className='flex flex-row flex-wrap items-center justify-center gap-[1rem] self-stretch text-center text-black'>
-//       {contactInfo.map((info, idx) => (
-//         <ContactCard key={idx} {...info} />
-//       ))}
-//     </div>
-//   );
-// }
-
-// Best so far
-
 export default function ContactDetails({ contactInfo }: ContactDetailsProps) {
   return (
     <div className='flex flex-row flex-wrap items-center justify-center gap-1 self-stretch text-center text-black md:flex-col'>
@@ -25,13 +13,3 @@ export default function ContactDetails({ contactInfo }: ContactDetailsProps) {
     </div>
   );
 }
-
-// export default function ContactDetails({ contactInfo }: ContactDetailsProps) {
-//   return (
-//     <div className='flex flex-row flex-wrap items-center justify-center gap-1 self-stretch px-4 text-center text-black md:flex-col'>
-//       {contactInfo.map((info, idx) => (
-//         <ContactCard key={idx} {...info} />
-//       ))}
-//     </div>
-//   );
-// }
