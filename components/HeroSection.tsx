237faa19865cba33160714259592c5756a29fa9c
Denis S Dujota <denisdujota@gmail.com>
2024-02-17 20:43:06 -0500

    fix buttoon styling

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -8,7 +8,7 @@ export default function HeroSection({ heroSection }: { heroSection: any }) {
       <article className='mx-auto max-w-[85rem]  px-6 sm:px-4 lg:px-8'>
         <div className='grid grid-cols-2 gap-4 md:grid-cols-1 md:items-center md:gap-8 xl:gap-20'>
           <div className='flex flex-col justify-center md:mt-5'>
-            <h1 className='text-gray-800 block text-6xl font-bold dark:text-white sm:!text-3xl md:self-center md:text-4xl lg:leading-tight'>
+            <h1 className='text-gray-800 e block text-6xl font-bold sm:!text-3xl md:self-center md:text-4xl lg:leading-tight'>
               <span className='heading'>{heroSection.heading}</span>
             </h1>
             <p className='text-gray-800 dark:text-gray-400 mt-3 text-lg sm:text-center md:self-center'>{heroSection.tagline}</p>
@@ -22,7 +22,7 @@ export default function HeroSection({ heroSection }: { heroSection: any }) {
                 <Image alt='arrow' width={24} height={24} src='/right-arrow.svg' />
               </Link>
               <Link
-                className='border-gray-200 text-gray-800 hover:bg-gray-50 dark:border-gray-700 dark:hover:bg-gray-800 dark:focus:ring-gray-600 inline-flex items-center justify-center gap-x-2 rounded-lg border bg-white px-4 py-3 text-sm font-medium shadow-sm disabled:pointer-events-none disabled:opacity-50 dark:bg-slate-900 dark:text-white dark:focus:outline-none dark:focus:ring-1'
+                className='border-gray-200 text-gray-800 hover:bg-gray-50  inline-flex items-center justify-center gap-x-2 rounded-lg border bg-white px-4 py-3 text-sm font-medium shadow-sm disabled:pointer-events-none disabled:opacity-50'
                 href='#'
               >
                 Contact sales team
