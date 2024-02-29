798ae0f2c95246f1f1cd0966afae67fae9f001cd
Denis S Dujota <denisdujota@gmail.com>
2024-02-29 01:36:20 -0500

    add msg to whatsapp

--- lib/constants.ts
+++ lib/constants.ts
@@ -17,14 +17,24 @@ export const footerNavAltLinks = [
 export const contactInfo = [
   { type: 'phone', icon: '/phone.svg', title: 'Phone Number', description: '+52 984 113 0567' },
   { type: 'email', icon: '/email.svg', title: 'Email Address', description: 'playacarrealty@gmail.com' },
-  { type: 'whatsapp', icon: '/whatsapp-white.svg', title: 'Our Location', description: 'WhatsApp', href: 'https://wa.me/529841130567' },
+  {
+    type: 'whatsapp',
+    icon: '/whatsapp-white.svg',
+    title: 'Our Location',
+    description: 'WhatsApp',
+    href: 'https://wa.me/529841130567?text=Hello%2C%20I%27m%20interested%20in%20learning%20more%20about%20your%20available%20properties%20and%20services.',
+  },
 ];
 export const logo = '/logo.png';
 
 export const socialIcons = [
   { label: 'Facebook', iconSrc: '/facebook.svg', href: 'https://www.facebook.com/homesplayacar' },
   { label: 'Instagram', iconSrc: '/instagram.svg', href: 'https://www.instagram.com/playacar_homes' },
-  { label: 'WhatsApp', iconSrc: '/whatsapp.svg', href: 'https://wa.me/529841130567' },
+  {
+    label: 'WhatsApp',
+    iconSrc: '/whatsapp.svg',
+    href: 'https://wa.me/529841130567?text=Hello%2C%20I%27m%20interested%20in%20learning%20more%20about%20your%20available%20properties%20and%20services.',
+  },
 ];
 
 export const currentYear = new Date().getFullYear();
